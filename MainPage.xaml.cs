5b00d527d58c32348198c2ddeb1c017099697a09
ilyaDavidov <ilya.davidov63@mail.ru>
2022-12-06 18:15:12 +0200

    q

---
+++ MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiApp7;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void DragGestureRecognizer_DragStarting(object sender, DragStartingEventArgs e)
+	{
+		if (e.Data.Properties.Count == 0)
+		{
+            e.Data.Properties.Add("qwe", (sender as DragGestureRecognizer).Parent);
+        }
+	}
+
+	private void DragGestureRecognizer_DropCompleted(object sender, DropCompletedEventArgs e)
+	{
+
+	}
+
+	private void DropGestureRecognizer_Drop(object sender, DropEventArgs e)
+	{
+
+		BoxView c= e.Data.Properties["qwe"] as BoxView;
+		(c.Parent as VerticalStackLayout).Clear();
+		((sender as DropGestureRecognizer).Parent as VerticalStackLayout).Add(c);
+
+    }
+
+	private void DropGestureRecognizer_DragOver(object sender, DragEventArgs e)
+	{
+
+	}
+
+	private void DropGestureRecognizer_DragLeave(object sender, DragEventArgs e)
+	{
+
+	}
+}
+
